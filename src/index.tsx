4b2c44016cf9d18f2e8b62d78210fb38578958c6
Duncan Mak <duncan.mak@xamarin.com>
2016-01-19 16:33:34 -0500

    Initial commit with Typescript, React, Webpack and working SourceMaps

---
+++ src/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class World extends React.Component<any, any> {
+    render() {
+        let content = "Hello World from a Class!";
+        return <h1>{content}</h1>;
+    }
+}
+
+interface Count { count: number }
+
+class Counter extends React.Component<Count, Count> {
+    constructor(props) {
+        super();
+        this.state = { count: props.count };
+    }
+
+    renderCount() {
+        return <h2>{this.state.count}</h2>;
+    }
+
+    renderButton(label, onClick) {
+        return <button onClick={onClick}>{label}</button>;
+    }
+
+    incrementCount() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    decrementCount() {
+        this.setState({count: this.state.count - 1});
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderButton('Increment', this.incrementCount.bind(this))}
+                {this.renderButton('Decrement', this.decrementCount)}
+                {this.renderCount()}
+           </div>
+        );
+    }
+}
+
+class Main extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <World />
+                <Counter count={100} />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Main />, document.getElementById('content'));
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'dist/bundle.js'
+  },
+
+  debug: true,
+  devtool: 'source-map',
+
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    loaders: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
